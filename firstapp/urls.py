33d53c6cad085dad28604d452f072540bf0e8524
aniketbhosale2808 <aniketbhosale2808@gmail.com>
2021-12-24 22:12:04 +0530

    Renders webpage from templates

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hello, name="hello")
+    path("", views.hello, name="hello"),
+    path("html/", views.hello_from_html,name="html"),
+
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("hello")
+    return HttpResponse("hello from httpresponse")
+
+def hello_from_html(request):
+    return render(request,"hello.html")
