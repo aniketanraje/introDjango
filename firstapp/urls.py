080cdb931e56b13dca8ff3b59ddef447a92bcfaa
aniketbhosale2808 <aniketbhosale2808@gmail.com>
2021-12-24 23:21:51 +0530

    Adds user input and returns as concatenated string and addition of numbers

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.hello, name="hello"),
-    path("html/", views.hello_from_html,name="html"),
+    path("html/", views.hello_from_html, name="html"),
+    path("html/add/", views.add, name="add"),
 
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def hello(request):
     return HttpResponse("hello from httpresponse")
 
+
 def hello_from_html(request):
-    return render(request,"hello.html")
+    return render(request, "hello.html")
+
+
+def add(request):
+    num1 = request.GET["num1"]
+    num2 = request.GET["num2"]
+    addition = int(num1) + int(num2)
+    result = num1 + num2
+    print("result:", result)
+    context = {'concatedstring': result, 'addition': addition}
+    return render(request, "result.html", context)
